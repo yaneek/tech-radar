e8f4dc7944e80c74dc5689757501847432d8cf7c
Grzegorz Marchwinski <yankeepl@gmail.com>
2019-01-29 19:55:47 +0100

    chore(build): add typescript support via react-create-app

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import ReactDOM from 'react-dom';
 import AppContainer from './components/AppContainer.jsx';
 import Description from './components/Description.jsx';
 
-const appWrapper = document.getElementById('app');
-appWrapper ? ReactDOM.render(<AppContainer radarId="radar"/>, appWrapper) : false;
-
-const descriptionWrapper = document.getElementById('description');
-descriptionWrapper ? ReactDOM.render(<Description />, descriptionWrapper) : false;
+ReactDOM.render(<AppContainer radarId="radar"/>, document.getElementById('app'))
+ReactDOM.render(<Description />, document.getElementById('description'));
